8880abf09ce7159004314593342474ab8bb0c905
Youssef mahmoud eid <ym1977@fayoum.edu.eg>
2025-08-09 04:54:43 +0300

    add styles_manager

--- lib/core/utilities/font_manager.dart
+++ lib/core/utilities/font_manager.dart
@@ -4,10 +4,7 @@ abstract class AppFontWieght {
   static FontWeight light = FontWeight.w300;
   static FontWeight regular = FontWeight.w400;
   static FontWeight medium = FontWeight.w500;
-  static FontWeight bold = FontWeight.w600;
-  static FontWeight semiBold = FontWeight.w700;
+  static FontWeight semiBold = FontWeight.w600;
+  static FontWeight bold = FontWeight.w700;
   static FontWeight extraBold = FontWeight.w800;
-  static FontWeight black = FontWeight.w900;
 }
-
-
---
+++ lib/core/utilities/styles_manager.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'font_manager.dart';
+
+abstract class AppTextStyles {
+  // Light - 300
+  static TextStyle textStyleLight12 = TextStyle(
+    fontWeight: AppFontWieght.light,
+  );
+  static TextStyle textStyleLight13 = TextStyle(
+    fontWeight: AppFontWieght.light,
+  );
+  static TextStyle textStyleLight14 = TextStyle(
+    fontWeight: AppFontWieght.light,
+  );
+
+  // Regular - 400
+  static TextStyle textStyleRegular12 = TextStyle(
+    fontWeight: AppFontWieght.regular,
+    fontSize: 12,
+  );
+  static TextStyle textStyleRegular14 = TextStyle(
+    fontWeight: AppFontWieght.regular,
+    fontSize: 14,
+  );
+  static TextStyle textStyleRegular16 = TextStyle(
+    fontWeight: AppFontWieght.regular,
+    fontSize: 16,
+  );
+  static TextStyle textStyleRegular20 = TextStyle(
+    fontWeight: AppFontWieght.regular,
+    fontSize: 20,
+  );
+  static TextStyle textStyleRegular24 = TextStyle(
+    fontWeight: AppFontWieght.regular,
+    fontSize: 24,
+  );
+
+  // Medium - 500
+
+  static TextStyle textStyleMedium14 = TextStyle(
+    fontWeight: AppFontWieght.medium,
+    fontSize: 14,
+  );
+  static TextStyle textStyleMedium16 = TextStyle(
+    fontWeight: AppFontWieght.medium,
+    fontSize: 16,
+  );
+  static TextStyle textStyleMedium18 = TextStyle(
+    fontWeight: AppFontWieght.medium,
+    fontSize: 18,
+  );
+  static TextStyle textStyleMedium20 = TextStyle(
+    fontWeight: AppFontWieght.medium,
+    fontSize: 20,
+  );
+  // semiBold - 600
+  static TextStyle textStyleSemiBold18 = TextStyle(
+    fontWeight: AppFontWieght.semiBold,
+    fontSize: 18,
+  );
+  static TextStyle textStyleSemiBold16 = TextStyle(
+    fontWeight: AppFontWieght.semiBold,
+    fontSize: 16,
+  );
+static TextStyle textStyleSemiBold20 = TextStyle(
+    fontWeight: AppFontWieght.semiBold,
+    fontSize: 20,
+  );
+  // bold - 700
+  static TextStyle textStyleBold20 = TextStyle(
+    fontWeight: AppFontWieght.bold,
+    fontSize: 20,
+  );
+  static TextStyle textStyleBold24 = TextStyle(
+    fontWeight: AppFontWieght.bold,
+    fontSize: 24,
+  );
+}

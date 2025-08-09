c79bfb6a3cd6244e04355ddc8ac0063983249734
Youssef mahmoud eid <ym1977@fayoum.edu.eg>
2025-08-09 04:14:35 +0300

    add app_colors app font_manager app images

---
+++ lib/core/utilities/app_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color mainColorStart = Color(0xff9D2235);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color black = Color(0xff000000);
+  static const Color accountype = Color(0xff1A1A1A);
+  static const Color acountsubtitle = Color(0xff666666);
+  static const Color semibrown = Color(0xff6B7280);
+  static const Color titletextfield = Color(0xff374151);
+  static const Color hinttextfield = Color(0xff9CA3AF);
+  static const Color hometusertitle = Color(0xff111827);
+  static const Color hometusersubtitle = Color(0xff4B5563);
+  static const Color searchfillcolor = Color(0xffF3F4F6);
+  static const Color adminprimarycolor = Color(0xffFF3B30);
+  static const Color backappbar = Color(0xff1F2937);
+  static const Color addcontant = Color(0xffF9FAFB);
+  static const Color adminpanelactivateicon = Color(0xffE53935);
+}

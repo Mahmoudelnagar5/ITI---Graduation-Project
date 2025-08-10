f6bd3244f4786a6816d4ebe98ad5e0f0e3c3fbf2
Mahmoud_Elnagar <elnagarm852@gmail.com>
2025-08-10 11:38:35 +0300

    add device preview for testing all devices and flutter_screenutil for responsive

--- lib/core/utilities/styles_manager.dart
+++ lib/core/utilities/styles_manager.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'font_manager.dart';
 
 abstract class AppTextStyles {
@@ -16,63 +17,63 @@ abstract class AppTextStyles {
   // Regular - 400
   static TextStyle textStyleRegular12 = TextStyle(
     fontWeight: AppFontWieght.regular,
-    fontSize: 12,
+    fontSize: 12.sp,
   );
   static TextStyle textStyleRegular14 = TextStyle(
     fontWeight: AppFontWieght.regular,
-    fontSize: 14,
+    fontSize: 14.sp,
   );
   static TextStyle textStyleRegular16 = TextStyle(
     fontWeight: AppFontWieght.regular,
-    fontSize: 16,
+    fontSize: 16.sp,
   );
   static TextStyle textStyleRegular20 = TextStyle(
     fontWeight: AppFontWieght.regular,
-    fontSize: 20,
+    fontSize: 20.sp,
   );
   static TextStyle textStyleRegular24 = TextStyle(
     fontWeight: AppFontWieght.regular,
-    fontSize: 24,
+    fontSize: 24.sp,
   );
 
   // Medium - 500
 
   static TextStyle textStyleMedium14 = TextStyle(
     fontWeight: AppFontWieght.medium,
-    fontSize: 14,
+    fontSize: 14.sp,
   );
   static TextStyle textStyleMedium16 = TextStyle(
     fontWeight: AppFontWieght.medium,
-    fontSize: 16,
+    fontSize: 16.sp,
   );
   static TextStyle textStyleMedium18 = TextStyle(
     fontWeight: AppFontWieght.medium,
-    fontSize: 18,
+    fontSize: 18.sp,
   );
   static TextStyle textStyleMedium20 = TextStyle(
     fontWeight: AppFontWieght.medium,
-    fontSize: 20,
+    fontSize: 20.sp,
   );
   // semiBold - 600
   static TextStyle textStyleSemiBold18 = TextStyle(
     fontWeight: AppFontWieght.semiBold,
-    fontSize: 18,
+    fontSize: 18.sp,
   );
   static TextStyle textStyleSemiBold16 = TextStyle(
     fontWeight: AppFontWieght.semiBold,
-    fontSize: 16,
+    fontSize: 16.sp,
   );
-static TextStyle textStyleSemiBold20 = TextStyle(
+  static TextStyle textStyleSemiBold20 = TextStyle(
     fontWeight: AppFontWieght.semiBold,
-    fontSize: 20,
+    fontSize: 20.sp,
   );
   // bold - 700
   static TextStyle textStyleBold20 = TextStyle(
     fontWeight: AppFontWieght.bold,
-    fontSize: 20,
+    fontSize: 20.sp,
   );
   static TextStyle textStyleBold24 = TextStyle(
     fontWeight: AppFontWieght.bold,
-    fontSize: 24,
+    fontSize: 24.sp,
   );
 }

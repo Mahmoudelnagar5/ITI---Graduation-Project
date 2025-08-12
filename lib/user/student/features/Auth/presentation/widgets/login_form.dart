dae864eb372a2beb156c2549859994387e685220
yousefa7med <youssefahmedserag@gmail.com>
2025-08-13 00:01:30 +0300

    refactor: update Auth forms to use SingleChildScrollView for better layout handling

--- lib/user/student/features/Auth/presentation/widgets/login_form.dart
+++ lib/user/student/features/Auth/presentation/widgets/login_form.dart
@@ -4,51 +4,53 @@ class LoginForm extends StatelessWidget {
   const LoginForm({super.key});
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24.0),
-      child: Form(
-        key: LoginCubit.get(context).key,
-        child: Column(
-          children: [
-            const TextFieldTitle(title: 'Email'),
-            const Gap(8),
-            AppTextFormField(
-              hintText: 'Email',
-              controller: LoginCubit.get(context).emailController,
-              validator: Validator.emailValidator(),
-            ),
-            const Gap(16),
-            const TextFieldTitle(title: 'Password'),
-
-            const Gap(8),
-
-            PasswordTextFormField(
-              hintText: 'Password',
-              controller: LoginCubit.get(context).passwordController,
-              validator: Validator.loginPasswordValidator(),
-            ),
-            const Gap(16),
-
-            const ForgotPasswordBotton(),
-            Gap(222.h),
-            AppBotton(
-              child: Text(
-                'Login',
-                style: AppTextStyles.textStyleMedium16.copyWith(
-                  color: Theme.of(context).colorScheme.onPrimary,
-                  fontFamily: AppFontFamily.roboto,
-                ),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Form(
+          key: LoginCubit.get(context).key,
+          child: Column(
+            children: [
+              const TextFieldTitle(title: 'Email'),
+              const Gap(8),
+              AppTextFormField(
+                hintText: 'Email',
+                controller: LoginCubit.get(context).emailController,
+                validator: Validator.emailValidator(),
               ),
-
-              onPressed: () {
-                final key = LoginCubit.get(context).key;
-
-                if (key.currentState!.validate()) {
-                  LoginCubit.get(context).login();
-                }
-              },
-            ),
-          ],
+              const Gap(16),
+              const TextFieldTitle(title: 'Password'),
+      
+              const Gap(8),
+      
+              PasswordTextFormField(
+                hintText: 'Password',
+                controller: LoginCubit.get(context).passwordController,
+                validator: Validator.loginPasswordValidator(),
+              ),
+              const Gap(16),
+      
+              const ForgotPasswordBotton(),
+              Gap(222.h),
+              AppBotton(
+                child: Text(
+                  'Login',
+                  style: AppTextStyles.textStyleMedium16.copyWith(
+                    color: Theme.of(context).colorScheme.onPrimary,
+                    fontFamily: AppFontFamily.roboto,
+                  ),
+                ),
+      
+                onPressed: () {
+                  final key = LoginCubit.get(context).key;
+      
+                  if (key.currentState!.validate()) {
+                    LoginCubit.get(context).login();
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/user/student/features/Auth/presentation/widgets/register_form.dart
+++ lib/user/student/features/Auth/presentation/widgets/register_form.dart
@@ -5,66 +5,70 @@ class RegisterForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24.0),
-      child: Form(
-        key: RegisterCubit.get(context).key,
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Form(
+          key: RegisterCubit.get(context).key,
 
-        child: Column(
-          children: [
-            const TextFieldTitle(title: 'Name'),
-            const Gap(8),
-            AppTextFormField(
-              hintText: 'Name',
-              controller: RegisterCubit.get(context).nameController,
-              validator: Validator.signupNameValidator(),
-            ),
-            const Gap(16),
-
-            const TextFieldTitle(title: 'Email'),
-            const Gap(8),
-            AppTextFormField(
-              hintText: 'Email',
-              controller: RegisterCubit.get(context).emailController,
-              validator: Validator.emailValidator(),
-            ),
-            const Gap(16),
-            const TextFieldTitle(title: 'Password'),
-            const Gap(8),
-            PasswordTextFormField(
-              hintText: 'Password',
-              controller: RegisterCubit.get(context).passwordController,
-              validator: Validator.signupPasswordValidator(),
-            ),
-            const Gap(16),
-            const TextFieldTitle(title: 'Confirm Password'),
-            const Gap(8),
-            PasswordTextFormField(
-              hintText: 'Confirm Password',
-              controller: RegisterCubit.get(context).confirmPasswordController,
-              validator: Validator.confirmPasswordValidator(
-                orgPasswordGetter: () =>
-                    RegisterCubit.get(context).passwordController.text,
+          child: Column(
+            children: [
+              const TextFieldTitle(title: 'Name'),
+              const Gap(8),
+              AppTextFormField(
+                hintText: 'Name',
+                controller: RegisterCubit.get(context).nameController,
+                validator: Validator.signupNameValidator(),
               ),
-            ),
-            Gap(70.h),
-            AppBotton(
-              child: Text(
-                'Register',
-                style: AppTextStyles.textStyleMedium16.copyWith(
-                  color: Theme.of(context).colorScheme.onPrimary,
-                  fontFamily: AppFontFamily.roboto,
+              const Gap(16),
+
+              const TextFieldTitle(title: 'Email'),
+              const Gap(8),
+              AppTextFormField(
+                hintText: 'Email',
+                controller: RegisterCubit.get(context).emailController,
+                validator: Validator.emailValidator(),
+              ),
+              const Gap(16),
+              const TextFieldTitle(title: 'Password'),
+              const Gap(8),
+              PasswordTextFormField(
+                hintText: 'Password',
+                controller: RegisterCubit.get(context).passwordController,
+                validator: Validator.signupPasswordValidator(),
+              ),
+              const Gap(16),
+              const TextFieldTitle(title: 'Confirm Password'),
+              const Gap(8),
+              PasswordTextFormField(
+                hintText: 'Confirm Password',
+                controller: RegisterCubit.get(
+                  context,
+                ).confirmPasswordController,
+                validator: Validator.confirmPasswordValidator(
+                  orgPasswordGetter: () =>
+                      RegisterCubit.get(context).passwordController.text,
                 ),
               ),
-              onPressed: () {
-                final key = RegisterCubit.get(context).key;
+              Gap(70.h),
+              AppBotton(
+                child: Text(
+                  'Register',
+                  style: AppTextStyles.textStyleMedium16.copyWith(
+                    color: Theme.of(context).colorScheme.onPrimary,
+                    fontFamily: AppFontFamily.roboto,
+                  ),
+                ),
+                onPressed: () {
+                  final key = RegisterCubit.get(context).key;
 
-                if (key.currentState!.validate()) {
-                  RegisterCubit.get(context).register();
-                }
-              },
-            ),
-          ],
+                  if (key.currentState!.validate()) {
+                    RegisterCubit.get(context).register();
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );

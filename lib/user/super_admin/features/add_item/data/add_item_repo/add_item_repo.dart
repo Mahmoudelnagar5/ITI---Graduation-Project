49f845f340d47c62587e567fc33dddcdd2e125eb
Mahmoud_Elnagar <elnagarm852@gmail.com>
2025-08-18 13:17:26 +0300

    Add update and delete functionality to AddItemRepo, allowing for item modification and removal in the Firestore database.

--- lib/user/super_admin/features/add_item/data/add_item_repo/add_item_repo.dart
+++ lib/user/super_admin/features/add_item/data/add_item_repo/add_item_repo.dart
@@ -8,6 +8,16 @@ abstract class AddItemRepo {
     required String description,
     required List<String> contents,
   });
+
+  Future<void> updateItem({
+    required String collectionName,
+    required String id,
+    required String title,
+    required String description,
+    required List<String> contents,
+  });
+
+  Future<void> deleteItem({required String collectionName, required String id});
 }
 
 class AddItemRepoImpl implements AddItemRepo {
@@ -32,6 +42,43 @@ class AddItemRepoImpl implements AddItemRepo {
     } catch (e) {
       debugPrint("Error adding track: $e");
       throw Exception("Failed to add track: $e");
+    }
+  }
+
+  @override
+  Future<void> updateItem({
+    required String collectionName,
+    required String id,
+    required String title,
+    required String description,
+    required List<String> contents,
+  }) async {
+    try {
+      await _firestore.collection(collectionName).doc(id).update({
+        'title': title,
+        'description': description,
+        'contents': contents,
+        'image': Assets.imagesFlutter,
+        // don't overwrite createdAt
+      });
+      debugPrint("Track updated successfully");
+    } catch (e) {
+      debugPrint("Error updating track: $e");
+      throw Exception("Failed to update track: $e");
+    }
+  }
+
+  @override
+  Future<void> deleteItem({
+    required String collectionName,
+    required String id,
+  }) async {
+    try {
+      await _firestore.collection(collectionName).doc(id).delete();
+      debugPrint("Track deleted successfully");
+    } catch (e) {
+      debugPrint("Error deleting track: $e");
+      throw Exception("Failed to delete track: $e");
     }
   }
 }
